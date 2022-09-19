73f1a9c318faff6a53d65c2455820303410809f2
William Isacsson <william.isacsson@gmail.com>
2022-09-19 15:33:07 +0200

    Movement
    Added Movement to the player and camera follow with CineMachine

--- FirstAssignment/Assets/Scripts/PlayerMovement.cs
+++ FirstAssignment/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,84 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //USED FOR MOVING
+    [Header("Movement")]
+    [SerializeField] float movementSpeed;
+    [SerializeField] float smoothInputSpeed;
+    Vector3 playerPosition;
+    Vector2 input;
+    Vector2 currentInput;
+    Vector2 velocity;
+
+    [Header("Jumping and Gravity")]
+    [SerializeField] float gravityValue;
+    [SerializeField] float jumpValue;
+    [SerializeField] bool isPlayerGrounded;
+
+    Rigidbody _rigidbody;
+    InputManager myInputManager;
+
+    void Awake()
     {
-        
+        myInputManager = new InputManager();
+        _rigidbody = GetComponent<Rigidbody>();
+
+        isPlayerGrounded = true;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        MoveCharacterWithKeyboard();
+        Jump();
+
+    }
+
+    void MoveCharacterWithKeyboard()
+    {
+        input = myInputManager.PlayerControlls.Movement.ReadValue<Vector2>();
+        currentInput = Vector2.SmoothDamp(currentInput, input, ref velocity, smoothInputSpeed);
+        playerPosition = new Vector3(currentInput.x, 0, currentInput.y);
+        transform.position += playerPosition * movementSpeed * Time.deltaTime;
+
+    }
+
+    void Jump()
+    {
+        if (myInputManager.PlayerControlls.Jump.triggered && isPlayerGrounded)
+        {
+            isPlayerGrounded = false;
+
+            _rigidbody.AddForce(Vector3.up * jumpValue, ForceMode.Impulse);
+
+        }
+
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isPlayerGrounded = true;
+        }
+
+
+    }
+
+
+
+    private void OnEnable()
+    {
+
+        myInputManager.Enable();
+
+    }
+
+    private void OnDisable()
+    {
+
+        myInputManager.Disable();
+
     }
 }
